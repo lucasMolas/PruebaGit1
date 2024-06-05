7412039a33b3e7c784ad539075f4d9f1ee1f64fa
LucasMolas <lucasmolas_@hotmail.com>
2024-06-05 16:48:13 -0300

    tercer commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("esto es bocaaaa");
+        int numero = 50;
+        System.out.println("todos putos");
+    }
+}
